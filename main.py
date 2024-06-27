05bcc275f6586026c3058ed5341973697dd07c77
JayFray <jayfray64@protonmail.com>
2024-06-27 16:26:38 -0500

    added a function to count words and a function to count the instances of each letter

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 def main():
-    book_path = "bookbot/books/frankenstein.txt"
+    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    count = count_words(text)
+    alphabet = count_letters(text)
+#    print(count)
+    print(alphabet)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def count_words(input_text):
+    words = input_text.split()
+    return len(words)
+#    count = 0
+#    for word in words:
+#        count += 1
+#    return count
+
+
+def count_letters(input_text):
+    chars = {}
+    for c in input_text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else: chars[lowered] = 1
+    return chars
 
 main()
